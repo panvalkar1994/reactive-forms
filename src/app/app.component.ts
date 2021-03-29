b02b46b6ff56e051fcd7fb5e61c3df8a53e582f8
vaibhav <vpanvalkar@outlook.com>
2021-03-29 12:00:22 +0530

    Set value using api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  registrationForm = new FormGroup({
+    userName: new FormControl(''),
+    password:new FormControl(''),
+    confirmPassword:new FormControl(''),
+    address:new FormGroup({
+      city:new FormControl(''),
+      state:new FormControl(''),
+      pincode:new FormControl('')
+    })
+  });
+
+  loadApiData(){
+    this.registrationForm.setValue({
+      userName :'Bruce',
+      password:'test',
+      confirmPassword:'test',
+      address:{
+        city:'Gotham',
+        state:'Gotham state',
+        pincode:'777666'
+      }
+    });
+  }
 }
