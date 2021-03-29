be2ac626cf9813362006d3052db7096fcd53de2d
vaibhav <vpanvalkar@outlook.com>
2021-03-29 17:15:39 +0530

    conditional validators for email subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { forbiddenNameValidator } from './shared/custom-form.validator';
 import { passwordValidator } from './shared/password.validator';
 
@@ -8,23 +8,44 @@ import { passwordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  registrationForm!:FormGroup;
+
   get userName(){
     return this.registrationForm.controls.userName;
+  }
+  get email(){
+    return this.registrationForm.controls.email;
   }
 
   constructor(private fb:FormBuilder){}
 
-  registrationForm = this.fb.group({
-    userName: ['', [Validators.required, Validators.minLength(3),forbiddenNameValidator(/password/)]],
-    password:[''],
-    confirmPassword:[''],
-    address:this.fb.group({
-      city:[''],
-      state:[''],
-      pincode:['']
-    })
-  }, {validator:passwordValidator})
+  ngOnInit(){
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3),forbiddenNameValidator(/password/)]],
+      password:[''],
+      confirmPassword:[''],
+      email:[''],
+      subscribe:[false],
+      address:this.fb.group({
+        city:[''],
+        state:[''],
+        pincode:['']
+      })
+    }, {validator:passwordValidator});
+
+    this.registrationForm.get('subscribe')?.valueChanges
+      .subscribe(checkedValue=>{
+        const email = this.registrationForm.controls.email;
+        if(checkedValue){
+          email.setValidators(Validators.required)
+        }else{
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      })
+
+  }
   // registrationForm = new FormGroup({
   //   userName: new FormControl(''),
   //   password:new FormControl(''),
@@ -41,6 +62,8 @@ export class AppComponent {
       userName :'Bruce',
       password:'test',
       confirmPassword:'test',
+      email:'',
+      subscribe:false,
       address:{
         city:'Gotham',
         state:'Gotham state',
